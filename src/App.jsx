5762d2edc9c275a4195a73b082e22f31e9a54b81
Aleksander Chromik <achromik@gmail.com>
2017-11-11 17:05:02 +0100

    All features
    Edit on native prompt()

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import './App.css';
 import CommentsListContainer from './CommentsListContainer.js'
 import CommentForm from './CommentFormContainer.js';
 
-import DevTools from './DevTools';
-
 const App = () => {
 	return (
 		<div className="App">
 			<CommentForm className='App-header'/>
 			<CommentsListContainer/>
-			<DevTools />
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
 
-//import { createStore, applyMiddleware } from "redux";
-//import { createLogger } from 'redux-logger';
-
 import { createStore  } from "redux";
-import DevTools from './DevTools';
 import reducer from './reducer';
 
-// import {createComment} from './actions'
-
-//const logger = createLogger();
 const store = createStore(
     reducer
-//    , applyMiddleware(logger)    
-    , DevTools.instrument()
 );
 
 ReactDOM.render(
@@ -26,10 +17,5 @@ ReactDOM.render(
         <App />
     </Provider>,
     document.getElementById('root'));
-
-
-    //store.dispatch(createComment('pierwszy komentarz'));
-    //store.dispatch(createComment('drugi komentarz'));
-    
 
 registerServiceWorker();
